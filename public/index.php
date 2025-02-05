3676c1ee8b25171fce4efcbef9f7eeaa8ca30dc2
Henry Vaughn <hcvaughn@unca.edu>
2025-02-05 16:41:21 -0500

    Update home page recipe link to use private_url_for()

--- public/index.php
+++ public/index.php
@@ -30,7 +30,7 @@ if($session->is_logged_in()) {
         <div class="cta-card">
             <h3>Share Recipes</h3>
             <p>Join our community and share your culinary creations with food enthusiasts worldwide.</p>
-            <a href="<?php echo url_for('/recipes/new.php?ref=home'); ?>" class="btn-text">Start Sharing</a>
+            <a href="<?php echo private_url_for('/recipes/new.php?ref=home'); ?>" class="btn-text">Start Sharing</a>
         </div>
         <div class="cta-card">
             <h3>Find Inspiration</h3>
