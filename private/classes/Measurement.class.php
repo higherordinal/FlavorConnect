a05f7be31a3b2ecaa37cccc380a91f4fd963cd47
Henry Vaughn <hcvaughn@unca.edu>
2025-02-11 17:26:27 -0500

    Update admin categories management:
    1. Convert categories page to use inline editing
    2. Add save changes button at bottom
    3. Create save.php to handle form submission
    4. Add validate_metadata function
    5. Update Measurement and RecipeAttribute classes to use common validation
    6. Remove abbreviation field from measurements

---
+++ private/classes/Measurement.class.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Measurement class for managing recipe measurements
+ * Extends DatabaseObject for database operations
+ */
+class Measurement extends DatabaseObject {
+    /** @var string Database table name */
+    static protected $table_name = 'measurement';
+    /** @var array Database columns */
+    static protected $db_columns = ['measurement_id', 'name'];
+    /** @var string Primary key column */
+    static protected $primary_key = 'measurement_id';
+
+    /** @var int Unique identifier for the measurement */
+    public $measurement_id;
+    /** @var string Name of the measurement */
+    public $name;
+
+    /**
+     * Constructor for Measurement class
+     * @param array $args Associative array of property values
+     */
+    public function __construct($args=[]) {
+        $this->measurement_id = $args['measurement_id'] ?? '';
+        $this->name = $args['name'] ?? '';
+    }
+
+    /**
+     * Validates the measurement data
+     * @return array Array of validation errors
+     */
+    protected function validate() {
+        return validate_metadata(['name' => $this->name]);
+    }
+
+    /**
+     * Gets all measurements ordered by name
+     * @return array Array of Measurement objects
+     */
+    public static function find_all_ordered() {
+        $sql = "SELECT * FROM " . static::$table_name . " ORDER BY name ASC";
+        return static::find_by_sql($sql);
+    }
+}
+?>
