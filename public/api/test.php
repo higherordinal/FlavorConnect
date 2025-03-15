2abbef7a0805f8ec69bb2e9f0468403e64e994b6
Henry Vaughn <hcvaughn@unca.edu>
2025-03-15 18:56:15 -0400

    Clean up: Remove unused API test files and simplify API calls

--- public/api/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Simple test file to check if API routing is working
-header('Content-Type: application/json');
-echo json_encode([
-    'success' => true,
-    'message' => 'API is working',
-    'time' => date('Y-m-d H:i:s')
-]);
-?>
