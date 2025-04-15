8f4df9ec87c68649028a4418fc7bb35d35ad0b71
Henry Vaughn <hcvaughn@unca.edu>
2025-04-15 12:14:01 -0400

    Update RecipeImageProcessor and core_utilities for improved routing and image handling

--- private/classes/RecipeImageProcessor.class.php
+++ private/classes/RecipeImageProcessor.class.php
@@ -15,7 +15,7 @@
  * - Provides comprehensive error handling and reporting
  * - Implements method chaining for fluent API usage
  * 
- * @author FlavorConnect Development Team
+ * @author Henry Vaughn
  */
 class RecipeImageProcessor {
     /** @var string Path to the source image */
